416fd165f59993a337b3b679fb2f181c7e8218ec
Kasper Yang <kasperyhr@gmail.com>
2018-06-18 15:57:23 +0800

    Problem 338

--- 338/Solution.java
+++ 338/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] countBits(int num) {
         int[] ret = new int[num + 1];
-        for(int i = 0; i <= num; i++){
-            ret[i] = Integer.bitCount(i);
+        for(int i = 1; i <= num; i++){
+            ret[i] = ret[i/2] + (i & 1);
         }
         return ret;
     }
