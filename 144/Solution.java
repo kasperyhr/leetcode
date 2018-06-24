2684da719bfb9191276c2cdba925517c973277b3
Kasper Yang <kasperyhr@gmail.com>
2018-06-24 12:29:45 +0800

    Problem 144

---
+++ 144/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList();
+        if(root == null)
+            return ret;
+        ret.add(root.val);
+        ret.addAll(preorderTraversal(root.left));
+        ret.addAll(preorderTraversal(root.right));
+        return ret;
+    }
+}
