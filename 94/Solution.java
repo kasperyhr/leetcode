8fefe9b5b7f6cee0996201543e8b974d36449ba0
Kasper Yang <kasperyhr@gmail.com>
2018-06-24 12:27:42 +0800

    Problem 94

---
+++ 94/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList();
+        if(root == null)
+            return ret;
+        ret.addAll(inorderTraversal(root.left));
+        ret.add(root.val);
+        ret.addAll(inorderTraversal(root.right));
+        return ret;
+    }
+}
