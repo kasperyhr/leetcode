2c0c826958106891695b83701c513006fb10283b
KasperYang <kasperyhr@gmail.com>
2018-11-04 11:00:03 -0500

    Problem 383

---
+++ 383/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for(int i = 0; i < magazine.length(); i++){
+            cnt[magazine.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < ransomNote.length(); i++){
+            cnt[ransomNote.charAt(i) - 'a']--;
+            if(cnt[ransomNote.charAt(i) - 'a'] < 0)
+                return false;
+        }
+        return true;
+        
+    }
+}
