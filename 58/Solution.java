5f30725e4b6d6cb5cc6dd83eab7810d6a7f409be
KasperYang <kasperyhr@gmail.com>
2018-12-17 17:03:24 -0500

    Problem 58

---
+++ 58/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        for(int i = str.length - 1; i >= 0; i--)
+            if(str[i].length() > 0)
+                return str[i].length();
+        return 0;
+    }
+}
