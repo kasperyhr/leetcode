7423c722bd3f27ec69fbbae8c5171227dfb00512
KasperYang <kasperyhr@gmail.com>
2018-12-17 22:51:54 -0500

    Problem 225

---
+++ 225/MyStack.java
@@ -0,0 +1,40 @@
+class MyStack {
+    private LinkedList<Integer> q;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q = new LinkedList<Integer>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q.addLast(x);
+        int size = q.size();
+        for(int i = 0; i < size - 1; i++){
+            q.addLast(q.remove());
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q.remove();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
