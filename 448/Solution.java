f15759911b0068b630341db002b52398a284995a
KasperYang <kasperyhr@gmail.com>
2019-03-05 14:34:59 -0500

    Problem 448

---
+++ 448/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ret = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            nums[Math.abs(nums[i]) - 1] = -(Math.abs(nums[Math.abs(nums[i]) - 1]));
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0) ret.add(i + 1);
+        }
+        return ret;
+    }
+}
